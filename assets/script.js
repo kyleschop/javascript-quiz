08758c800bb400a75fe58612e1279a8121b4694c
Kyle Schoppmann <klparuu@yahoo.com>
2022-12-01 18:18:19 -0700

    Adjusted script.js file

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,42 @@
-var time = document.getElementById("time");
-var card = document.getElementById("card");
-var buttonDiv = document.createElement("div");
-var questionsBox = document.createElement("div");
-var startBtn = document.createElement("button");
+var card = document.getElementById('card');
+var cardHeading = document.createElement('h1');
+var cardQ = document.createElement('p');
+var cardBtn = document.createElement('div');
+var cardDisplay = document.createElement('p');
+
+var questionsArray = [
+    "JavaScript data types include numbers, strings, boolean values, and objects.", 
+    "A declared variable that does not have a value is undefined.",
+    "Global variables must be declared inside a function.",
+    "Prompt and alert allow the user to enter input.",
+    "'1' == 1",
+    "'True' === True",
+    "The result of 2+2+'2'=42"
+];
+
+card.appendChild(cardHeading);
+card.appendChild(cardQ);
+card.appendChild(cardBtn);
+card.appendChild(cardDisplay);
+
 var trueBtn = document.createElement("button");
 var falseBtn = document.createElement("button");
-var secondsLeft = 90;
-
-//Quiz Questions
-var q1 = "JavaScript data types include numbers, strings, boolean values, and objects.";
-var q2 = "A declared variable that does not have a value is undefined.";
-var q3 = "Global variables must be declared inside a function.";
-var q4 = "Prompt and alert allow the user to enter input.";
-var q5 = "'1' == 1";
-var q6 = "'True' === True";
-var q7 = "The result of 2+2+'2'=42"
-
-time.textContent = "00";
-card.textContent = "This is a timed quiz. Press start to begin.";
-startBtn.textContent = "START!";
 trueBtn.textContent = "True"
 falseBtn.textContent = "False"
-
-startBtn.setAttribute("style", "width:80px; margin:20px; border:none; border-radius:10px; background-color:rgb(29, 255, 17); cursor:pointer");
 trueBtn.setAttribute("style", "width:80px; margin:20px; border:none; border-radius:10px; background-color:rgb(29, 255, 17); cursor:pointer");
 falseBtn.setAttribute("style", "width:80px; margin:20px; border:none; border-radius:10px; background-color:rgb(29, 255, 17); cursor:pointer");
 
-card.appendChild(buttonDiv);
-buttonDiv.appendChild(startBtn);
+var time = document.getElementById("time");
+time.textContent = "00";
+var secondsLeft = 90;
+
+var startBtn = document.createElement("button");
+startBtn.textContent = "START!";
+startBtn.setAttribute("style", "width:80px; margin:20px; border:none; border-radius:10px; background-color:rgb(29, 255, 17); cursor:pointer");
+
+var secondsLeft = 90;
+cardHeading.textContent = 'This is a timed quiz. Press start to begin.';
+cardBtn.appendChild(startBtn);
 
 //The count down timer
 function startTime(){
@@ -42,47 +51,91 @@ function startTime(){
     }, 1000);
 };
 
-function correct() {
-    card.textContent = "Correct";
+function nextQ() {
+    console.log('it worked');
 }
 
-function incorrect() {
-    card.textContent = "Incorrect";
-}
-
-function question1(){
-    card.textContent = "True or False?"
-    card.appendChild(questionsBox);
-    card.appendChild(buttonDiv);
-    buttonDiv.textContent = " ";
-    buttonDiv.appendChild(trueBtn);
-    buttonDiv.appendChild(falseBtn)
-    questionsBox.textContent = q1
-    questionsBox.setAttribute("style", "font-weight:bold")
-
-    if(trueBtn.addEventListener("click", correct)) {
-        console.log(correct)
-    } else if (falseBtn.addEventListener("click", incorrect));
+function correctAnswers(){
+    console.log('yes');
+};
+function incorrectAnswers(){
+    console.log('no');
 };
 
-function question2(){
-    card.textContent = "True or False?"
-    card.appendChild(questionsBox);
-    card.appendChild(buttonDiv);
-    buttonDiv.textContent = " ";
-    buttonDiv.appendChild(trueBtn);
-    buttonDiv.appendChild(falseBtn)
-    questionsBox.textContent = q2
-    questionsBox.setAttribute("style", "font-weight:bold")
+function questionSetUp() {
+	cardHeading.textContent = "True or False?";
+    cardBtn.textContent = ' ';
+    cardBtn.appendChild(trueBtn);
+	cardBtn.appendChild(falseBtn);
+    
+};
 
-    if(trueBtn.addEventListener("click", question2)) {
-    } else if (falseBtn.addEventListener("click", question2));
+function askQ7() {
+    var setUp = questionSetUp();
+    cardQ.textContent = questionsArray[6];
+    trueBtn.addEventListener('click', correctAnswers);
+    trueBtn.addEventListener('click', nextQ);
+    falseBtn.addEventListener('click', incorrectAnswers);
+    falseBtn.addEventListener('click', nextQ);
+};
 
+function askQ6() {
+    var setUp = questionSetUp();
+    cardQ.textContent = questionsArray[5];
+    trueBtn.addEventListener('click', incorrectAnswers);
+    trueBtn.addEventListener('click', askQ7);
+    falseBtn.addEventListener('click', correctAnswers);
+    falseBtn.addEventListener('click', askQ7);
+};
+
+function askQ5() {
+    var setUp = questionSetUp();
+    cardQ.textContent = questionsArray[4];
+    trueBtn.addEventListener('click', correctAnswers);
+    trueBtn.addEventListener('click', askQ6);
+    falseBtn.addEventListener('click', incorrectAnswers);
+    falseBtn.addEventListener('click', askQ6);
+};
+
+function askQ4() {
+    var setUp = questionSetUp();
+    cardQ.textContent = questionsArray[3];
+    trueBtn.addEventListener('click', incorrectAnswers);
+    trueBtn.addEventListener('click', askQ5);
+    falseBtn.addEventListener('click', correctAnswers);
+    falseBtn.addEventListener('click', askQ5);
+};
+
+function askQ3() {
+    var setUp = questionSetUp();
+    cardQ.textContent = questionsArray[2];
+    trueBtn.addEventListener('click', incorrectAnswers);
+    trueBtn.addEventListener('click', askQ4);
+    falseBtn.addEventListener('click', correctAnswers);
+    falseBtn.addEventListener('click', askQ4);
+};
+
+function askQ2() {
+    var setUp = questionSetUp();
+    cardQ.textContent = questionsArray[1];
+    trueBtn.addEventListener('click', correctAnswers);
+    trueBtn.addEventListener('click', askQ3);
+    falseBtn.addEventListener('click', incorrectAnswers);
+    falseBtn.addEventListener('click', askQ3);
+};
+
+function askQ1() {
+    var setUp = questionSetUp();
+    cardQ.textContent = questionsArray[0];
+    trueBtn.addEventListener('click', correctAnswers);
+    trueBtn.addEventListener('click', askQ2);
+    falseBtn.addEventListener('click', incorrectAnswers);
+    falseBtn.addEventListener('click', askQ2);
 };
 
 function beginQuiz() {
     startTime();
-    question1();
+    askQ1();
 };
 
-startBtn.addEventListener("click", beginQuiz);
+startBtn.addEventListener('click', beginQuiz);
